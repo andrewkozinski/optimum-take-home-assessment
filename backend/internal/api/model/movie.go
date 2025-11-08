bb5588b3ea0105e6f5dc1b57d4a666889a5e2bb7
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-07 20:55:41 -0500

    Created movie struct to hold movie data

---
+++ backend/internal/api/model/movie.go
@@ -0,0 +1,10 @@
+package model
+
+// Movie represents a response from TMDB API for a movie
+type Movie struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Overview    string `json:"overview"`
+	PosterPath  string `json:"poster_path"`
+	ReleaseDate string `json:"release_date"`
+}

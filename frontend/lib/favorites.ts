7b7ed5605c45c5e72e58015aa9f63094ea88a6e3
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-08 15:18:09 -0500

    Added clear favorite movies function and comment to remove favorite movie function

--- frontend/lib/favorites.ts
+++ frontend/lib/favorites.ts
@@ -4,7 +4,7 @@
  * Gets favorite movies from local storage
  * @returns Favorite movies from local storage
  */
-export function getFavoriteMovies(): string[] {
+export function getFavoriteMovies(): number[] {
     if (typeof window === 'undefined') return [];
     const favorites = localStorage.getItem('favoriteMovies');
     return favorites ? JSON.parse(favorites) : [];
@@ -15,7 +15,7 @@ export function getFavoriteMovies(): string[] {
  * @param movieId TMDB API Movie ID
  * @returns True if the movie is favorited, false otherwise
  */
-export function isMovieFavorited(movieId: string): boolean {
+export function isMovieFavorited(movieId: number): boolean {
     const favorites = getFavoriteMovies();
     return favorites.includes(movieId);
 }
@@ -24,7 +24,7 @@ export function isMovieFavorited(movieId: string): boolean {
  * Adds a movie to favorites
  * @param movieId TMDB API Movie ID
  */
-export function addFavoriteMovie(movieId: string): void {
+export function addFavoriteMovie(movieId: number): void {
     const favorites = getFavoriteMovies();
     if (!favorites.includes(movieId)) {
         favorites.push(movieId);
@@ -32,11 +32,22 @@ export function addFavoriteMovie(movieId: string): void {
     }
 }
 
-export function removeFavoriteMovie(movieId: string): void {
+/**
+ * Removes a movie from favorites
+ * @param movieId TMDB API Movie ID
+ */
+export function removeFavoriteMovie(movieId: number): void {
     const favorites = getFavoriteMovies();
     const index = favorites.indexOf(movieId);
     if (index !== -1) {
         favorites.splice(index, 1);//remove the movie from favorites, starting at index "index", remove 1 item
         localStorage.setItem('favoriteMovies', JSON.stringify(favorites));
     }
+}
+
+/**
+ * Clears all favorite movies from local storage
+ */
+export function clearFavoriteMovies(): void {
+    localStorage.removeItem('favoriteMovies');
 }

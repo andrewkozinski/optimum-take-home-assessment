5f3a6d1bdcc81d31d788e93024816ade21db0b40
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-11-09 00:34:16 -0500

    Added moduleMapper to resolve jest related errors

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -8,4 +8,8 @@ module.exports = {
   transform: {
     ...tsJestTransformCfg,
   },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
 };
